16428fd12c15b48abec69415f93295ec3202c499
Algebra-FUN <worldcreator@foxmail.com>
2020-07-26 11:12:18 +0800

    Updates

---
+++ precache-manifest.294445624d9ec0b298ae46d04733d92c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7dce53f57e2de0674e494f4900d0d93",
+    "url": "/ShapeALot/index.html"
+  },
+  {
+    "revision": "b13bb8234b430106478f",
+    "url": "/ShapeALot/static/css/main.ef7c0d01.chunk.css"
+  },
+  {
+    "revision": "14e9f8318682e0e54cd3",
+    "url": "/ShapeALot/static/js/2.a1a393b2.chunk.js"
+  },
+  {
+    "revision": "7baac2e68c562348b516173f26c107b7",
+    "url": "/ShapeALot/static/js/2.a1a393b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13bb8234b430106478f",
+    "url": "/ShapeALot/static/js/main.5f648eac.chunk.js"
+  },
+  {
+    "revision": "f25b874c33ab8897229c",
+    "url": "/ShapeALot/static/js/runtime-main.429cd5e0.js"
+  }
+]);
